bc2dc03e1d605ee7f6f42c400b7154d49f8b64a8
Ariket Gupta <ariketgupta@gmail.com>
2026-07-19 20:30:43 +0530

    basic setup is done

--- vision_engine/main.py
+++ vision_engine/main.py
@@ -2,4 +2,21 @@ import cv2
 import numpy as np
 import pandas as pd
 
-img = cv2.imread()
+cap = cv2.VideoCapture(0)
+
+while True:
+    success , frame = cap.read()
+
+    if not success :
+        print("unable to open camera")
+        break
+
+    cv2.rectangle(frame , (100 , 100) , (200 , 200) ,(255 , 255 , 255)  , 4)
+
+    cv2.imshow("face selected image",frame)
+
+    if cv2.waitKey(1) & 0xff == ord('q') :
+        break
+
+cap.release()
+cv2.destroyAllWindows()
